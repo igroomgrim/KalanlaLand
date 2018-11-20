59b904fa7327e7418612785e53fa2340083cd261
Anak Mirasing <thaihooligan@hotmail.com>
2018-11-20 15:53:28 +0700

    Update tests for buy/sell case and refactoring some part of method in contract

--- contracts/Kalanlaland.sol
+++ contracts/Kalanlaland.sol
@@ -11,7 +11,7 @@ contract KalanlaLand is ERC721Token {
     uint256 price;
   }
 
-  Token[] tokens;
+  Token[] public tokens;
   mapping (uint256 => bool) public tokensForSell;
 
   constructor() ERC721Token("KalanlaLand", "KLT") public {
@@ -29,12 +29,10 @@ contract KalanlaLand is ERC721Token {
   }
 
   // Create Token
-  function createToken(string _name, uint _price, address _owner) public onlyGameOwner() returns (uint256) {
-    require(_price > 0 ether);
-
+  function createToken(string _name, address _owner) public onlyGameOwner() returns (uint256) {
     Token memory _token = Token({
       name: _name,
-      price: _price
+      price: 0
     });
 
     uint256 tokenID = tokens.push(_token) - 1;
@@ -59,7 +57,7 @@ contract KalanlaLand is ERC721Token {
     tokens[_tokenID].price = 0;
   }
 
-  function buyItem(uint256 _tokenID) public payable {
+  function buyToken(uint256 _tokenID) public payable {
     require(msg.sender != ownerOf(_tokenID));
     require(msg.value >= tokens[_tokenID].price);
     require(!_isContract(msg.sender));
@@ -83,7 +81,8 @@ contract KalanlaLand is ERC721Token {
   }
 
   function _transferToken(address _from, address _to, uint256 _tokenID) internal {
-    transferFrom(_from, _to, _tokenID);
+    removeTokenFrom(_from, _tokenID);
+    addTokenTo(_to, _tokenID);
   }
 
   // Utils
--- test/KalanlaLand.js
+++ test/KalanlaLand.js
@@ -19,5 +19,54 @@ contract('KalanlaLand', async (accounts) => {
 	     	let _gameOwner = await instance.gameOwner();
 	     	assert.equal(_gameOwner, gameOwner);
 		})
+
+		it("should gameOwner create token to the land", async () => {
+			await instance.createToken("TOKEN_0", gameOwner, {
+				from: gameOwner
+			});
+
+			let tokenOwner = await instance.ownerOf(0);
+			assert.equal(gameOwner, tokenOwner);
+
+			let totalSupply = await instance.totalSupply.call();
+			assert.equal(1, totalSupply);
+
+			let item0 = await instance.tokens(0);
+			assert.equal("TOKEN_0", item0[0]);
+			assert.equal(0, item0[1]);
+
+			let balance = await instance.balanceOf(gameOwner);
+			assert.equal(1, balance);
+		})
+	})
+
+	describe("Buy/Sell", () => {
+		it("should gameOwner set tokens[0] for sell", async () => {
+			await instance.offerTokenForSell(0, 1 * ether, {
+				from: gameOwner
+			});
+
+			let item0IsForSell = await instance.tokensForSell(0);
+			assert.equal(item0IsForSell, true);
+
+			let item0 = await instance.tokens(0);
+			assert.equal(1 * ether, item0[1]);
+		})
+
+		it("should player1 buy tokens[0] from the market(gameOwner)", async () => {
+			await instance.buyToken(0, {
+				from: player1,
+				value: 1.1 * ether
+			});
+
+			let tokenOwner = await instance.ownerOf(0);
+			assert.equal(player1, tokenOwner);
+
+			let balanceOfGameOwner = await instance.balanceOf(gameOwner);
+			assert.equal(0, balanceOfGameOwner);
+
+			let balanceOfPlayer1 = await instance.balanceOf(player1);
+			assert.equal(1, balanceOfPlayer1);
+		})
 	})
 })
